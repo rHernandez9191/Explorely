b67954c6e7109f87b207e7ca8c6b4f9d3592a3a6
sraskz <sarita081204segura@gmail.com>
2023-11-30 16:07:39 -0600

    con formularios tentativcos

--- Public/js/Controllers/LoginControllers.js
+++ Public/js/Controllers/LoginControllers.js
@@ -28,10 +28,10 @@ const ValidarInputs = (pUser, pPass) => {
 const  RedireccionarUsuario = (PersonaDB) => {
     let nombreRol = ObtenerRol(PersonaDB.Rol);
     if (nombreRol == 'Client') {
-        location.href = 'IndexCliente.html';
+        location.href = 'paginaBusqueda.html';
     }
     if (nombreRol == 'Admin') {
-        location.href = 'IndexAdmin.html';
+        location.href = 'indexAdminProvisional.html';
     }
 };
 
