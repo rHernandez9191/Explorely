daff0fab9e459b71d0b3448873d84213e18f0e7d
rHernandez9191 <149850567+rHernandez9191@users.noreply.github.com>
2023-12-03 20:53:42 -0600

    Iniciar session funcional

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,32 +6,6 @@ const SocioRoute = require("./SocioRoute");
 
 module.exports = (app) => {
     app.use("/api", PersonaRoute);
-
-
-    //Manejar rutas no encontradas devolviendo 404
-    app.use((req, res) => {
-        res.status(404);
-        res.send({
-            error: {
-                status: 404,
-                message: 'Ruta no encontrada'
-            }
-        });
-    });
-
-    //Manejar errores
-    app.use((err, req, res, next) => {
-        res.status(err.status || 500);
-        res.send({
-            error: {
-                status: err.status || 500,
-                message: err.message
-            }
-        });
-    });
-}
-
-module.exports = (app) => {
     app.use("/api", SocioRoute);
 
 
@@ -57,3 +31,9 @@ module.exports = (app) => {
         });
     });
 }
+
+
+    
+
+
+    //Manejar rutas no encontradas devolviendo 404
