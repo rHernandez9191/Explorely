0e66de46d2171ace2a96aa741a42f4d11e670933
rHernandez9191 <149850567+rHernandez9191@users.noreply.github.com>
2023-12-03 22:13:53 -0600

    agregar al carrito boton no funcional

--- Public/js/Controllers/GestionarPaginaBusqueda.js
+++ Public/js/Controllers/GestionarPaginaBusqueda.js
@@ -37,8 +37,9 @@ GetListaSocios();
            </div>
            <div class="lugar">
             <div class="local"><span class="txtlugar">Precio: </span>${listaPersonas[i].Precio}</div>
-            <button id="botonAgregar">Agregar al carrito</button>
+           
            </div>
+           <button id="botonAgregar">Agregar al carrito</button>
           </div>
          </div>
          `;
